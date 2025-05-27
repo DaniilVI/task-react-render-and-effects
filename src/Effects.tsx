e0027a6e271b2d6a4c962e330a8aa25fac8554c9
DaniilVI <vahtanovdaniil@gmail.com>
2025-05-27 22:11:46 +0300

    jsx

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,5 +1,38 @@
 import { subscribe, unsubscribe } from './resources/API';
+import ReactDOM from 'react-dom';
+import { useState, useEffect } from 'react';
+
+function getMessage(sourceId: string) {
+    const [lastMessage, setLastMessage] = useState('-1');
+
+    useEffect(() => {
+        setLastMessage('-1');
+
+        const handleNewMessage = function (message: any) {
+            setLastMessage(message);
+        };
+
+        subscribe(sourceId, handleNewMessage);
+
+        return () => {
+            unsubscribe(sourceId, handleNewMessage);
+        };
+    }, [sourceId]);
+
+    return { sourceId, lastMessage };
+}
 
 export function Effects(props: { sourceId: string }) {
-    return <div>123</div>;
+    const { sourceId, lastMessage } = getMessage(props.sourceId);
+
+    return (
+        <div>
+            {sourceId}: {lastMessage}
+        </div>
+    );
 }
+
+// ReactDOM.render(
+//      <Effects sourceId="-1"/>,
+//      document.getElementById('root')
+//    );
